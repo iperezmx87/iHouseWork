25717b9a85c42a0df0c261aa33b7f7f48ee97770
Israel Pérez Saucedo <neomatrixisra25@hotmail.com>
2018-04-11 23:32:52 -0500

    Carga Inicial
    Implementación en ASP.NET MVC muy básica de la API de Google GSuite Spreadsheets API.
    
    Más información en https://developers.google.com/sheets/api/quickstart/dotnet?hl=es

---
+++ GoogleSpreadsheetTest/GoogleSpreadsheetTest/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace GoogleSpreadsheetTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateGoogleSpreadsheet()
+        {
+            // generando la hoja de google spreadsheet
+            SheetsService shService = GenerateGoogleService();
+
+            // Define request parameters.
+            String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            String range = "Class Data!A2:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    shService.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                Console.WriteLine("Name, Major");
+                foreach (var row in values)
+                {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    //  Console.WriteLine("{0}, {1}", row[0], row[4]);
+                }
+            }
+            else
+            {
+            }
+            return View();
+        }
+
+        private SheetsService GenerateGoogleService()
+        {
+            UserCredential credential;
+
+            // usa tu propio archivo jejeje
+            using (var stream =
+                new FileStream(Server.MapPath("client_secret.json"), FileMode.Open, FileAccess.Read))
+            {
+                string credPath = @"C:\api\.credentials\sheets.googleapis.com-dotnet-quickstart.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Crear el servicio de sheet de Google
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // devolver la instancia del servicio de google sheet
+            return service;
+        }
+    }
+}
